f74d201a67a8684996630f7457f95c2a971c89f7
IgorVeshkin <IgorVeshkin.official@yandex.ru>
2023-06-14 00:20:17 +0300

    Simcard CRUD Management System Commit

--- SimcardCRUD/static/js/Simcard_CRUD.js
+++ SimcardCRUD/static/js/Simcard_CRUD.js
@@ -1,36 +1,8 @@
-// $('#myModal').on('shown.bs.modal', function () {
-//   $('#myInput').trigger('focus')
-// })
 
 var myModal = document.getElementById('myModal');
 var myInput = document.getElementById('myInput');
 
-// window.onload=function(){
-//     var mb = document.getElementById("b");
-//     mb.addEventListener("click", handler);
-//     mb.addEventListener("click", handler2);
-// }
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
-
-
-// var myDropdown_item = document.getElementById('dropdown-item');
-// var ServiceType_input = document.getElementById('ServiceType-name');
-
-// function hello(inputtext) {
-// 	alert("It is working");
-// 	alert(ServiceType_input);
-// 	ServiceType_input.value = "Super";
-// 	alert(myDropdown_item);
-// }
-
-
-
-// document.getElementById("dropdown-item").addEventListener("click", hello);
-
-// myDropdown_item.onclick = function(){
-// 	alert("It is working");
-// 	// ServiceType_input.setAttribute('value', myDropdown_item.innerHTML);
-// };

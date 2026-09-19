624a9d8091274e5cf40c5a6168db7dd254e8d128
IgorVeshkin <IgorVeshkin.official@yandex.ru>
2026-09-19 20:33:45 +0300

    Rewritten views.py and template for Sim Card CRUD-system

--- SimcardCRUD/CRUD_System/views.py
+++ SimcardCRUD/CRUD_System/views.py
@@ -16,22 +16,19 @@ from CRUD_System.tasks import send_client_creation_message
 @login_required(login_url='login-request')
 def main(request):
 
-    if request.method == "GET":
-        if "deleted-record-id" in request.GET:
-            Simcard.objects.filter(pk=request.GET.get('deleted-record-id')).delete()
+    if request.method == "POST":
 
-            return redirect('/')
+        # Добавление записи пользователя
+        if request.POST.get("action") == "create":
+            current_tariff = TariffPlan.objects.filter(Title=request.POST.get("add-record-ServiceType"))
 
-        elif "add-record-IMEI" in request.GET:
-            current_tariff = TariffPlan.objects.filter(Title=request.GET.get("add-record-ServiceType"))
+            client_name = request.POST.get("add-record-CustomerName")
+            client_phone = request.POST.get('add-record-Phone')
 
-            client_name = request.GET.get("add-record-CustomerName")
-            client_phone = request.GET.get('add-record-Phone')
-
-            SimcardRecord = Simcard(IMEI=request.GET.get('add-record-IMEI'),
+            SimcardRecord = Simcard(IMEI=request.POST.get('add-record-IMEI'),
                                     PhoneNumber=client_phone,
                                     ClientName=client_name,
-                                    RegistrationDate=request.GET.get("add-record-RegDate"),
+                                    RegistrationDate=request.POST.get("add-record-RegDate"),
                                     TariffPlan=current_tariff[0])
 
             SimcardRecord.save()
@@ -41,23 +38,52 @@ def main(request):
 
             print(f"Задача успешно отправлена с id: {task_result.id}")
 
+            print("Запись клиента успешно создана...")
+
             return redirect('/')
 
-        elif "update-record-IMEI" in request.GET:
 
-            current_tariff = TariffPlan.objects.filter(pk=request.GET.get("update-record-ServiceType-pk"))
+        # Обновление записи пользователя
+        if request.POST.get("action") == "update":
+            current_tariff = TariffPlan.objects.filter(pk=request.POST.get("update-record-ServiceType-pk"))
 
-            current_simcard_record = Simcard.objects.get(id=request.GET.get('update-record-simcard-pk'))
+            current_simcard_record = Simcard.objects.get(id=request.POST.get('update-record-simcard-pk'))
 
-            current_simcard_record.IMEI = request.GET.get('update-record-IMEI')
-            current_simcard_record.PhoneNumber = request.GET.get('update-record-Phone')
-            current_simcard_record.ClientName = request.GET.get('update-record-CustomerName')
-            current_simcard_record.RegistrationDate = request.GET.get('update-record-RegDate')
+            current_simcard_record.IMEI = request.POST.get('update-record-IMEI')
+            current_simcard_record.PhoneNumber = request.POST.get('update-record-Phone')
+            current_simcard_record.ClientName = request.POST.get('update-record-CustomerName')
+            current_simcard_record.RegistrationDate = request.POST.get('update-record-RegDate')
             current_simcard_record.TariffPlan = current_tariff[0]
 
             current_simcard_record.save()
 
+            print("Запись клиента успешно обновлена...")
+
             return redirect('/')
+
+
+        # Удаление записи пользователя
+        if request.POST.get("action") == "delete":
+            deleted_record_id = request.POST.get("deleted-record-id")
+
+            if not deleted_record_id:
+                return HttpResponseBadRequest(
+                    "ID клиента не был передан на удаление"
+                )
+
+            deleted_count, _ = Simcard.objects.filter(
+                pk=deleted_record_id
+            ).delete()
+
+            if deleted_count == 0:
+                return HttpResponseBadRequest(
+                    "Запись с указанным ID не найдена"
+                )
+    
+            print("Запись клиента успешно удалена...")
+
+            return redirect('/')
+            
 
     context = {"Simcards": Simcard.objects.all(),
                "TariffPlan": TariffPlan.objects.all()}
